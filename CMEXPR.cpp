2c327904a9e5eab0730c7ffd919d5d1c106a1f3b
MarcinKozak005 <49290219+MarcinKozak005@users.noreply.github.com>
2019-08-07 16:34:50 +0200

    Not finished version v2

--- CMEXPR.cpp
+++ CMEXPR.cpp
@@ -13,6 +13,7 @@ int priority(char x)
         case '+':
         case '-': result = 1; break;
         case '*':
+        case '\':
         case '/': result = 2; break;
         default: result = 3;
     }
@@ -21,29 +22,35 @@ int priority(char x)
 
 int delete_parenthesis(int begin, string& str)
 {
+    //cout<<"-1:"<<str<<endl;
     int lowest_priority = 4;
     int i;
-    for(i=begin; i<str.length(); ++i)
+    for(i=begin+1; i<str.length(); ++i)
     {
-        if(str[i] == '(') i = delete_parenthesis(i+1,str)-1;
+        if(str[i] == '(') i = delete_parenthesis(i,str)-1;
         else if(str[i] == ')') break;
-        else //*!*
-        {
-            if( priority(str[i]) < lowest_priority) lowest_priority = priority(str[i]);
-        }
     }
     
+    int end = i;
+    
+    for(int i=begin+1;i<end;++i)
+    {
+        if( priority(str[i]) < lowest_priority) lowest_priority = priority(str[i]);
+    }
+    
+    
     int op_prior = lowest_priority;
-    int op_p_prior;
-    int op_n_prior;
+    int op_p_prior=-1;
+    int op_n_prior=-1;
     char op_p;
     char op_n;
-    int end = i;
-    bool next,prev;
+
+    bool next=false;
+    bool prev=false;
     
-    if(begin-2 >= 0)
+    if(begin-1 >= 0)
     {
-        op_p = str[begin-2];
+        op_p = str[begin-1];
         op_p_prior = priority(op_p);
     }
     if(end+1 < str.length())
@@ -51,40 +58,52 @@ int delete_parenthesis(int begin, string& str)
         op_n = str[end+1];
         op_n_prior = priority(op_n);
     }
-    
-    
-    /*if(op_prior >= op_p_prior && op_prior >= op_n_prior && (op_p != '/' && op_p != '-') )
-    {
-        str.erase(end,1);
-        str.erase(begin-1,1);
-        return end-1;
-    }
-    */
+        
     if(end+1 < str.length() && op_prior >= op_n_prior) next = true;
     if(end+1 >= str.length()) next=true;
-    if(begin-2 >= 0)
+    if(begin-1 >= 0)
     {
         if(op_p =='/') prev = false;
         else
         {
             if(op_prior>op_p_prior) prev = true;
-            if(op_prior=op_p_prior)
+            if(op_prior==op_p_prior)
             {
+                //cout<<"In that if"<<endl;
                 if(op_p!='-') prev=true;
                 else prev=false;
             }
         }
     }
-    if(begin-2<0) prev=true;
+    if(begin-1<0) prev=true;
     
-    if(next==true && prev==true)
-    {
+    
+    
+    // cout<<" 0:"<<str<<endl;
+    // cout<<"-----"<<endl;
+    // cout<<"op_prior: "<<op_prior<<endl;
+    // cout<<"op_p: "<<op_p<< " op_p_prior: "<<op_p_prior<<endl;
+    // cout<<"op_n: "<<op_n<<" op_n_prior: "<<op_n_prior<<endl;
+    // cout<<"-----"<<endl;
+    
+    
+    if(begin+2==end)
+    {   
         str.erase(end,1);
-        str.erase(begin-1,1);
+        str.erase(begin,1);
         return end-1;
     }
     
+    //cout<<prev<<" "<<next<<endl;
+    if(next==true && prev==true)
+    {
+        str.erase(end,1);
+        str.erase(begin,1);
+        //cout<<" 1:"<<str<<endl;
+        return end-1;
+    }
     
+    //cout<<" 2:"<<str<<endl;
     return end+1;
 }
 
@@ -104,5 +123,15 @@ int main()
         cout<<str<<endl;
     }
     
+    /*string a="a-(e/f)";
+    
+    for(int i=0;i<a.length();++i)
+    {
+        if(a[i]=='(') i=delete_parenthesis(i,a);
+    }
+    cout<<a<<endl;*/
+    
+    
+        
     return 0;
 }
