db3dec03916f1e95214299fb9b8c40abdc5b4404
MarcinKozak005 <49290219+MarcinKozak005@users.noreply.github.com>
2019-08-07 18:25:34 +0200

    With helpful couts

--- CMEXPR.cpp
+++ CMEXPR.cpp
@@ -13,72 +13,68 @@ int priority(char x)
         case '+':
         case '-': result = 1; break;
         case '*':
-        case '\':
         case '/': result = 2; break;
         default: result = 3;
     }
     return result;
 }
 
-int delete_parenthesis(int begin, string& str)
+int delete_parentheses(int start, string& str)
 {
-    //cout<<"-1:"<<str<<endl;
-    int lowest_priority = 4;
     int i;
-    for(i=begin+1; i<str.length(); ++i)
+    for(i=start+1; i<str.length(); ++i)
     {
-        if(str[i] == '(') i = delete_parenthesis(i,str)-1;
+        if(str[i] == '(') i = delete_parentheses(i,str)-1;
         else if(str[i] == ')') break;
     }
-    
     int end = i;
     
-    for(int i=begin+1;i<end;++i)
+    int lowest_priority = 4;            // 4 is not accessible while calling priority()
+    int counter=0;                      // counts the nestings of parentheses
+    for(int i=start+1;i<end;++i)
     {
-        if( priority(str[i]) < lowest_priority) lowest_priority = priority(str[i]);
+        if(str[i]=='(') counter++;
+        else if(str[i]==')') counter--;
+        else if( priority(str[i]) < lowest_priority && counter==0) lowest_priority = priority(str[i]);
     }
     
-    
     int op_prior = lowest_priority;
-    int op_p_prior=-1;
-    int op_n_prior=-1;
     char op_p;
     char op_n;
-
+    int op_p_prior=-1;                  //priority of the previous operator (befor the current parentheses)
+    int op_n_prior=-1;                  //priority of the next operator (after the current parentheses)
+    
     bool next=false;
     bool prev=false;
     
-    if(begin-1 >= 0)
+    if(start-1 >= 0)
     {
-        op_p = str[begin-1];
+        op_p = str[start-1];
         op_p_prior = priority(op_p);
+        
+        if(op_p =='/') prev = false;
+        else
+        {
+            if(op_prior > op_p_prior) prev=true;
+            if(op_prior == op_p_prior)
+            {
+                //cout<<"In that if"<<endl;
+                if(op_p != '-') prev=true;
+                else prev=false;
+            }
+        }
     }
     if(end+1 < str.length())
     {
         op_n = str[end+1];
         op_n_prior = priority(op_n);
+        if(op_prior >= op_n_prior) next = true;
     }
-        
-    if(end+1 < str.length() && op_prior >= op_n_prior) next = true;
+    
     if(end+1 >= str.length()) next=true;
-    if(begin-1 >= 0)
-    {
-        if(op_p =='/') prev = false;
-        else
-        {
-            if(op_prior>op_p_prior) prev = true;
-            if(op_prior==op_p_prior)
-            {
-                //cout<<"In that if"<<endl;
-                if(op_p!='-') prev=true;
-                else prev=false;
-            }
-        }
-    }
-    if(begin-1<0) prev=true;
+    if(start-1 < 0) prev=true;
     
-    
-    
+    //Good for Debuging
     // cout<<" 0:"<<str<<endl;
     // cout<<"-----"<<endl;
     // cout<<"op_prior: "<<op_prior<<endl;
@@ -87,10 +83,10 @@ int delete_parenthesis(int begin, string& str)
     // cout<<"-----"<<endl;
     
     
-    if(begin+2==end)
+    if(start+2==end)
     {   
         str.erase(end,1);
-        str.erase(begin,1);
+        str.erase(start,1);
         return end-1;
     }
     
@@ -98,7 +94,7 @@ int delete_parenthesis(int begin, string& str)
     if(next==true && prev==true)
     {
         str.erase(end,1);
-        str.erase(begin,1);
+        str.erase(start,1);
         //cout<<" 1:"<<str<<endl;
         return end-1;
     }
@@ -113,25 +109,16 @@ int main()
     int n;
     string str;
     cin>>n;
-    for(int i=0;i<n;++i)
+    
+    for(int i=0; i<n; ++i)
     {
         cin>>str;
-        for(int j=0;j<str.length();++j)
+        for(int j=0; j<str.length(); ++j)
         {
-            if(str[j]=='(') j=delete_parenthesis(j,str);   
+            if(str[j]=='(') j=delete_parentheses(j,str);   
         }
         cout<<str<<endl;
     }
-    
-    /*string a="a-(e/f)";
-    
-    for(int i=0;i<a.length();++i)
-    {
-        if(a[i]=='(') i=delete_parenthesis(i,a);
-    }
-    cout<<a<<endl;*/
-    
-    
         
     return 0;
 }
