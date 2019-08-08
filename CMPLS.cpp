0453727ed616b3994eb812c051caa8545858ce2d
MarcinKozak005 <49290219+MarcinKozak005@users.noreply.github.com>
2019-08-08 22:47:19 +0200

    Final form

--- CMPLS.cpp
+++ CMPLS.cpp
@@ -1,92 +1,123 @@
-// Example program
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void show(vector<int> & vec)
+void show(vector<vector<int>> & holder)
 {
-    for(int i=0;i<vec.size();++i)
-    {
-        cout<<vec[i]<<endl;   
-    }
-}
-
-void show2(vector<vector<int>> & holder)
-{
+    int size = holder.size();
+    int min = holder[size-1].size();
     
-    int min=holder[holder.size()-1].size();
-    
-    //cout<<min<<endl;
-    for(int i=0;i<min;++i)
+    //show the rectangular block
+    for(int i=0; i<min; ++i)
     {
-        for(int j=0;j<holder.size();++j)
+        for(int j=0;j<size;++j)
         {
             cout<<holder[j][i]<<" ";
         }
         cout<<endl;
     }
-    for(int i=min;i<holder[0].size();++i)
+    
+    //show the triangular rest
+    for(int i=min; i<holder[0].size(); ++i)
     {
-        for(int j=0;j<holder.size()-i+min-1;++j)
+        for(int j=0; j<size-i+min-1; ++j)
         {
             cout<<holder[j][i]<<" ";   
         }
         cout<<endl;
     }
-    
 }
 
 
-void fill(vector<vector<int>> & holder)
+void count_table(vector<vector<int>> & holder)
 {
     vector<int> helper;
     bool identical=true;
-    int current;
+    int difference;
     
-    for(int i=0;i<holder[0].size();++i)
+    for(int i=0; i<holder[0].size(); ++i)
     {
-        for(int j=0;j<holder[i].size()-1;++j)
+        for(int j=0; j<holder[i].size()-1; ++j)
         {
-            current=holder[i][j+1]-holder[i][j];
-            helper.push_back(current);
-            if(current!=helper[0]) identical=false;
+            difference=holder[i][j+1]-holder[i][j];
+            helper.push_back(difference);
+            if(difference!=helper[0]) identical=false;
         }
         holder.push_back(helper);
-        if(identical) break; cout<<"break"<<endl;
+        
+        //we reached the "end" earlier
+        if(identical) break;
         identical=true;
+        
         helper.clear();
     }
 }
 
 void add_diagonal(vector<vector<int>> & holder)
 {
-    // show(holder[0]);
-    // show(holder[1]);
-    // show(holder[2]);
+    int size = holder.size();
     
-    int x;
-    for(int i=holder.size()-1;i>=0;--i)
+    //adds the constant element to last vector
+    holder[size-1].push_back( holder[size-1][0] );
+    
+    int size_of_i;
+    for(int i=size-2;i>=0;--i)
     {
-        if(i!=holder.size()-1) x=holder[i+1][holder[i].size()-1];
-        else x=holder[holder.size()-1][0];
-        holder[i].push_back(holder[i][holder[i].size()-1]+x);
-        break;
+        size_of_i = holder[i].size();
+        holder[i].push_back( holder[i][size_of_i-1] + holder[i+1][size_of_i-1] );
     }
 }
 
-
+void parse_input(int n)
+{
+    int in,out;
+    int tmp;
+    
+    
+    for(int i=0; i<n; ++i)
+    {
+        cin>>in>>out;
+        
+        if(in==1)
+        {
+            cin>>tmp;
+            for(int j=0;j<out;++j)  cout<<tmp<<" ";   
+            cout<<endl;
+        }
+        else
+        {
+            vector<vector<int>> holder;
+            vector<int> input_sequence;
+            
+            for(int j=0;j<in;++j)
+            {
+                cin>>tmp;
+                input_sequence.push_back(tmp);
+            }
+            
+            holder.push_back(input_sequence);
+            count_table(holder);
+            
+            for(int k=0;k<out;++k)
+            {
+                add_diagonal(holder);
+            }
+            
+            //printing result in desired form
+            for(int l=in;l<in+out;++l)
+            {
+                cout<<holder[0][l]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+}
 int main()
 {
-    vector<vector<int>> holder;
-    
-    vector<int> exp={1,4,9,16,25,36};
-    holder.push_back(exp);
-    fill(holder);
-    show2(holder);
-    add_diagonal(holder);
-    show2(holder);
-    //cout<<holder.size()<<endl;
+    int n;
+    cin>>n;
+    parse_input(n);
     
     return 0;
 }
