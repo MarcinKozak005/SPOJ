b7aa02cd0102226f72eb1a671b7f346b24869e1b
MarcinKozak005 <49290219+MarcinKozak005@users.noreply.github.com>
2019-08-13 14:20:32 +0200

    Create CPTTRN3.cpp

---
+++ Basics/CPTTRN3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+void pattern(int lines, int columns)
+{
+	for(int i=0;i<3*lines+1;++i)
+	{
+		for(int j=0;j<3*columns+1;++j)
+		{
+		    if((i%3==0)||(j%3==0)) cout<<"*";
+		    else cout<<".";
+		}
+		cout<<endl;
+	}
+}
+
+void parse_input(int n)
+{
+	int lines, columns;
+	for(int i=0;i<n;++i)
+	{
+	    cin>>lines;
+	    cin>>columns;
+		pattern(lines,columns);
+		cout<<endl;
+	}
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	parse_input(n);
+	
+	return 0;
+}
