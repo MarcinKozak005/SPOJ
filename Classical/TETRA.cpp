4612691dd73d496dc8add7552183817af00ab1be
MarcinKozak005 <49290219+MarcinKozak005@users.noreply.github.com>
2019-08-14 23:13:03 +0200

    Finished version of TETRA.cpp

--- Classical/TETRA.cpp
+++ Classical/TETRA.cpp
@@ -1,8 +1,6 @@
-// Example program
 #include <iostream>
-#include <string>
-#include <iomanip>
-#include <math.h>
+#include <iomanip>  //for setprecision
+#include <math.h>   //for sqrt
 
 using namespace std;
 
@@ -15,8 +13,10 @@ double calculate_the_determinant(double a1,double b1,double c1,double d1,double 
     double e=e1*e1;
     double f=f1*f1;
     
-    double sum1 = (a-d+e)*(c-f-a+e)*(f-b+e)+(b-f+e)*(d-a+e)*(c-d-b+e)+2*e*(e-d-a)*(e-b-f);
-    double sum2 = (d-a+e)*(e-b-f)*(a-d+e)+(f-b+e)*(b-f+e)*(e-d-a)+2*e*(c-f-a+e)*(c-d-b+e);
+    //That two sums come from transforming the determinant shown at the wikipedia site (https://wikimedia.org/api/rest_v1/media/math/render/svg/54b6240b89a1f296099a6aca3fed341265e41207)
+    // Yes, I could have used the formula shown below (https://wikimedia.org/api/rest_v1/media/math/render/svg/463da03fbc47d54e5e5f9cd60438a74281421e44) but I didn't :D
+    double sum1 = (a-e+c)*(d-f-a+c)*(f-b+c)+(b-f+c)*(e-a+c)*(d-e-b+c)+2*c*(c-e-a)*(c-b-f);
+    double sum2 = (e-a+c)*(c-b-f)*(a-e+c)+(f-b+c)*(b-f+c)*(c-e-a)+2*c*(d-f-a+c)*(d-e-b+c);
     
     return sum1-sum2;
 }
@@ -33,32 +33,25 @@ double area_heron(double x, double y, double z)
     return sqrt(perimiter*(perimiter-x)*(perimiter-y)*(perimiter-z));
 }
 
-// void parse_input(int n)
-// {
-//     double a,b,c,d,e,f;
-//     for(int i=0;i<n;++i)
-//     {
-//         cin>>a>>b>>c>>d>>e>>f;
+void parse_input(int n)
+{
+    double a,b,c,d,e,f;
+    double radius;
+    for(int i=0;i<n;++i)
+    {
+        cin>>a>>b>>c>>d>>e>>f;
     
-//     double radius = calculate_the_determinant(a,b,c,d,e,f);
-//     //double radius = 3*calculate_the_volume(calculate_the_determinant(a,b,c,d,e,f)) / ( area_heron(a,b,c)+area_heron(a,b,d)+area_heron(a,d,c)+area_heron(d,b,c) );
-//     cout/*<<setprecision(4)*/<<radius<<endl;
-//     }
-// }
+        radius = 3*calculate_the_volume(calculate_the_determinant(a,b,c,d,e,f)) / ( area_heron(a,b,d)+area_heron(a,c,e)+area_heron(b,c,f)+area_heron(d,e,f) );
+        cout<<fixed<<setprecision(4)<<radius<<endl;
+    }
+}
 
 int main()
 {
-    double a,b,c,d,e,f;
-    
-    cin>>a>>b>>c>>d>>e>>f;
-    double radius = calculate_the_determinant(a,b,c,d,e,f);
-    //double radius = 3*calculate_the_volume(calculate_the_determinant(a,b,c,d,e,f)) / ( area_heron(a,b,c)+area_heron(a,b,d)+area_heron(a,d,c)+area_heron(d,b,c) );
-    cout/*<<setprecision(4)*/<<radius<<endl;
-    
-    // int n;
-    // cin>>n;
-    // parse_input(n);
-    
+
+    int n;
+    cin>>n;
+    parse_input(n);
     
     return 0;
 }
