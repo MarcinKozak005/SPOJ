7dae59c704da8210589f21212ec72ecf85151925
MarcinKozak005 <49290219+MarcinKozak005@users.noreply.github.com>
2019-08-16 14:31:28 +0200

    Not working version

--- Classical/ARITH.cpp
+++ Classical/ARITH.cpp
@@ -1,27 +1,28 @@
-#include <iostream>     //basic one
-#include <string.h>     //for string
-#include <vector>       //for vector class
-#include <algorithm>    //for max function
+#include <iostream>
+#include <string.h>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-//Defined below the main()
-void parseInput(int input_number);
+
+
+//below
 void add(string &fst, string &snd, string & res);
-void sub(string &fst, string &snd, string & res);
-void mul(string &fst, string &snd, string &res, vector<string> & str_vec);
-void print_add_sub(char sign, string & fst, string &snd, string &res);
-void print_mul(string & fst, string &snd, string &res, vector<string> str_vec);
-
-void print_spaces(int num)
+void add1(string &fst, string &snd)
 {
-    for(int i=0;i<num;i++) cout<<" ";    
+    string tmp;
+    add(fst,snd,tmp);
+    fst=tmp;
 }
-
-void print_dash(int num)
+void mul(string &fst, string &snd, string &res, vector<string> & str_vec);
+void mul1(string &fst, string &snd)
 {
-    for(int i=0;i<num;i++) cout<<"-";
-    cout<<"\n";
+    string tmp;
+    vector<string> str_vec;
+    mul(fst,snd,tmp,str_vec);
+    fst=tmp;
 }
 
 void str_reverse(string& str)
@@ -29,68 +30,36 @@ void str_reverse(string& str)
     reverse(str.begin(),str.end());
 }
 
-void delete_leading_0s(string & str)
+string factorial(string & n)
 {
-    while(str[0]=='0')
+    string result="1";
+    string one="1";
+    add1(n,one);
+    for(string i="1";i!=n;add1(i,one))
     {
-        str.erase(0,1);
+        mul1(result,i);
     }
+    
+    return result;
 }
 
+void parse_input(int n)
+{
+    string x;
+    for(int i=0;i<n;++i)
+    {
+        cin>>x;
+        cout<<factorial(x)<<endl;
+    }
+}
 
 int main()
 {
-
-    int input_number;
-    cin>>input_number;
+    int n;
+    cin>>n;
+    parse_input(n);
     
-    parseInput(input_number);
-   
     return 0;
-}
-
-void parseInput(int input_number)
-{
-    for(int i=0; i<input_number; i++)
-    {
-        string input;
-        cin>>input;
-        
-        //Splitting input into: first (argument), sign (operation), second (argument)
-        int field = 0;
-        char sign;
-        while(sign = input[field])
-        {
-            if(sign > 47) field++;
-            else break;
-        }
-        string first = input.substr(0,field);
-        string second = input.substr(field+1);
-        //end of Splitting
-      
-        string result;
-    
-        if (sign == '+')
-        {
-            add(first,second,result);
-            print_add_sub(sign,first,second,result);
-        }
-        else if (sign == '-')
-        {
-            sub(first,second,result);
-            delete_leading_0s(result);
-            print_add_sub(sign,first,second,result);
-        }
-        else // sign == '*'
-        {
-            vector<string> partial_results;
-            
-            mul(first,second,result,partial_results);
-            print_mul(first,second,result,partial_results);
-        }
-        
-        cout<<"\n\n";
-    }
 }
 
 void add(string &fst, string &snd, string & res)
@@ -117,45 +86,6 @@ void add(string &fst, string &snd, string & res)
       }
       if (overflow!=0) res.push_back(overflow+48);
       str_reverse(res);
-}
-
-void sub(string &fst, string &snd, string & res)
-{
-    res.erase(0);
-    // sub function modify the fst string so wee need a copy
-    string fst_copy(fst); 
-    int pos1 = fst.length()-1;
-    int pos2 = snd.length()-1;
-    
-    for(;pos1>=0 || pos2>=0;pos1--,pos2--)
-       {
-           if(pos2>=0)
-           {
-               if (fst_copy[pos1]>=snd[pos2]) res.push_back(fst_copy[pos1] - snd[pos2] + 48);
-               else if (fst_copy[pos1]<snd[pos2])
-               {
-                   for(int i=(pos1-1); i>=0; i--)
-                   {
-                       if(fst_copy[i]!='0')
-                       {
-                           fst_copy[i]--;
-                           break;
-                       }
-                       else
-                       {
-                           fst_copy[i]='9';
-                       }
-                   }
-                   fst_copy[pos1] +=10;
-                   res.push_back(fst_copy[pos1] - snd[pos2] + 48);
-               }
-           }
-           else //fst.len() > snd.length()
-           {
-               res.push_back(fst_copy[pos1]);
-           }
-       }
-       str_reverse(res);
 }
 
 void mul(string &fst, string &snd, string &res, vector<string> & str_vec)
@@ -219,57 +149,4 @@ void mul(string &fst, string &snd, string &res, vector<string> & str_vec)
            str_vec[i].erase(str_vec[i].length()-i);
         }
        
-}
-
-void print_add_sub(char sign, string & fst, string &snd, string &res)
-{
-    int max_length = max( max(fst.length(),snd.length()+1), res.length() );
-    int max_top_bar = max(snd.length()+1,res.length());
-  
-    print_spaces(max_length - fst.length());
-    cout<<fst<<endl;
-    print_spaces(max_length - snd.length() -1);
-    cout<<sign;
-    cout<<snd<<endl;
-    print_spaces(max_length-max_top_bar);
-    print_dash(max_top_bar);
-    print_spaces(max_length - res.length());
-    cout<<res;
-  
-}
-
-void print_mul(string & fst, string &snd, string &res, vector<string> str_vec)
-{
-    int max_length = max( max(fst.length(),snd.length()+1), res.length() );
-    int max_top_bar = max( snd.length() +1, str_vec[0].length() );
-    int max_bottom_bar;
-    
-    print_spaces(max_length-fst.length());
-    cout<<fst<<endl;
-    print_spaces(max_length - snd.length() -1);
-    cout<<'*';
-    cout<<snd<<endl;
-    
-    if(snd.length()!=1)
-    {
-        max_bottom_bar = max( res.length(), str_vec[str_vec.size()-1].length() );
-        print_spaces(max_length-max_top_bar);
-        print_dash(max_top_bar);
-        for(int i=0;i<str_vec.size();i++)
-        {
-            print_spaces(max_length-i-str_vec[i].length());
-            cout<<str_vec[i]<<endl;
-        }
-        print_spaces(max_length-max_bottom_bar);
-        print_dash(max_bottom_bar);
-    }
-    else //there is only 1 partial result -> different result printing
-    {
-        max_bottom_bar = max(snd.length()+1,res.length());
-        print_spaces(max_length-max_bottom_bar);
-        print_dash(max_bottom_bar);
-    }
-    
-    print_spaces(max_length-res.length());
-    cout<<res<<endl;
 }
